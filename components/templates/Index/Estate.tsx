0b367fa39912138311ea6a4d1878983aeeb1cb75
soheil-jalili <jalilisoheil14@gmail.com>
2025-10-24 19:24:01 -0700

    add estate and estate item

--- components/templates/Index/Estate.tsx
+++ components/templates/Index/Estate.tsx
@@ -1,10 +1,34 @@
 import Title from "@/components/module/Title";
 import React from "react";
+import EstateItem from "./EstateItem";
 
 const Estate: React.FC = () => {
   return (
     <section className="container">
-      <Title title="در سقفینو دنبال چه نوع ملکی هستید" className="mt-22 mb-8"/>
+      <Title title="در سقفینو دنبال چه نوع ملکی هستید" className="mt-22 mb-8" />
+
+      <div className="grid grid-cols-4 gap-6">
+        <EstateItem
+          image="/assets/images/estate/image1.png"
+          price="28.900"
+          title="خانه مسکونی"
+        />
+        <EstateItem
+          image="/assets/images/estate/image2.png"
+          price="309.798"
+          title="آپارتمان و برج"
+        />
+        <EstateItem
+          image="/assets/images/estate/image3.png"
+          price="946"
+          title="ویلا"
+        />
+        <EstateItem
+          image="/assets/images/estate/image4.png"
+          price="27.339"
+          title="تجاری و اداری"
+        />
+      </div>
     </section>
   );
 };
--- components/templates/Index/FeatureItem.tsx
+++ components/templates/Index/FeatureItem.tsx
@@ -18,10 +18,18 @@ const FeatureItem: React.FC<FeatureItemType> = ({
   route,
 }: FeatureItemType) => {
   return (
-    <div className="h-[511px] rounded-2xl p-8 bg-gray-2 flex flex-col justify-between">
+    <div className="h-[511px] rounded-2xl p-8 bg-gray-2 flex flex-col justify-between outline -outline-offset-1 outline-gray-4">
       <div className="text-center">
-        <Image src={image} width={280} height={145} alt={title} className="mb-6" />
-        <h5 className="mb-3 font-bold font-bold-shabnam text-xl text-gray-11">{title}</h5>
+        <Image
+          src={image}
+          width={280}
+          height={145}
+          alt={title}
+          className="mb-6"
+        />
+        <h5 className="mb-3 font-bold font-bold-shabnam text-xl text-gray-11">
+          {title}
+        </h5>
         <p className="text-gray-10 text-sm">{description}</p>
       </div>
       <Link href={route} className="primary__btn">
