e4f1f870036b68c5227e62b89d8e387aef9be7dd
soheil-jalili <jalilisoheil14@gmail.com>
2025-10-30 12:45:29 -0700

    add login and login backdrop

---
+++ components/module/Header/LoginBack.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Login from "./Login";
+
+type LoginBackType = {
+  show: boolean;
+  closeLoginHandler: () => void;
+};
+
+const LoginBack: React.FC<LoginBackType> = ({
+  show,
+  closeLoginHandler,
+}: LoginBackType) => {
+  if (!show) return null;
+  return (
+    <div
+      className="fixed inset-0 bg-black/60 z-50 flex items-center justify-center"
+      onClick={closeLoginHandler} 
+    >
+      <div
+        onClick={(e) => e.stopPropagation()} 
+        className="rounded-2xl"
+      >
+        <Login />
+      </div>
+    </div>
+  );
+};
+
+export default LoginBack;
--- components/module/Header/Nav.tsx
+++ components/module/Header/Nav.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { HiMiniBars3 } from "react-icons/hi2";
 import MobileMenu from "./MobileMenu";
+import LoginBack from "./LoginBack";
 
 const Nav: React.FC = () => {
   const [sticky, setSticky] = useState(false);
   const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const [isOpenLoginModel, setIsOpenLoginModel] = useState<boolean>(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,10 +20,20 @@ const Nav: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const openLoginHandler = () => {
+    setIsOpenLoginModel(true);
+  };
+  
+  const closeLoginHandler = () => {
+    setIsOpenLoginModel(false);
+  };
+
   return (
     <nav
       className={`${
-        sticky ? "w-full! top-0 shadow-md" : "container w-full md:top-10 md:rounded-2xl"
+        sticky
+          ? "w-full! top-0 shadow-md"
+          : "container w-full md:top-10 md:rounded-2xl"
       } bg-white flex items-center fixed left-0 right-0 z-50 h-14.5 md:h-[115px] *:text-gray-10 px-4 md:px-4 lg:px-8 transition-all duration-300 justify-between max-[267px]:px-2 w-full md:w-[90%]`}
     >
       <HiMiniBars3
@@ -58,12 +70,13 @@ const Nav: React.FC = () => {
       </ul>
 
       <div className="flex items-center gap-4 lg:gap-9 max-[267px]:hidden">
-        <Link
-          href="/signin"
-          className="hidden md:block text-base hover:text-gray-11 transition-all"
+        <button
+          onClick={openLoginHandler}
+          className="cursor-pointer hidden md:block text-base hover:text-gray-11 transition-all"
         >
           ورود
-        </Link>
+        </button>
+
         <Link
           href="/add-post"
           className="border border-primary px-4 py-2  rounded-lg text-primary text-xs sm:text-sm font-medium-shabnam!"
@@ -73,6 +86,7 @@ const Nav: React.FC = () => {
       </div>
 
       <MobileMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
+      <LoginBack show={isOpenLoginModel} closeLoginHandler={closeLoginHandler}  />
     </nav>
   );
 };
