9b6ee3908c54e52df17c738067fbc1ece70e9650
soheil-jalili <jalilisoheil14@gmail.com>
2025-10-30 14:54:28 -0700

    add Accept Code component

---
+++ components/module/Header/AcceptCode.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { GoClock } from "react-icons/go";
+
+const AcceptCode: React.FC = () => {
+  const acceptCodeHandler = () => {};
+  return (
+    <div>
+      <div className="flex justify-center flex-col items-center">
+        <h4 className="font-bold-shabnam! mb-10 text-2xl text-gray-12">
+          کد تائید
+        </h4>
+        <p className="text-base text-gray-11 mb-1">
+          کد ارسال‌شده به شماره ۰۹۱۲۳۳۳۴۴۵۵ را وارد کنید
+        </p>
+        <button className="text-gray-9 text-base cursor-pointer mb-6">
+          ویرایش شماره موبایل
+        </button>
+
+        <div className="flex gap-5 mb-4 *:rounded-lg *:outline-0 *:text-center">
+          <input
+            type="number"
+            className="appearance-none w-[91.2px] h-12 border border-gray-7 focus:shadow-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield]"
+          />
+
+          <input
+            type="number"
+            className="appearance-none w-[91.2px] h-12 border border-gray-7 focus:shadow-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield]"
+          />
+
+          <input
+            type="number"
+            className="appearance-none w-[91.2px] h-12 border border-gray-7 focus:shadow-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield]"
+          />
+
+          <input
+            type="number"
+            className="appearance-none w-[91.2px] h-12 border border-gray-7 focus:shadow-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield]"
+          />
+
+          <input
+            type="number"
+            className="appearance-none w-[91.2px] h-12 border border-gray-7 focus:shadow-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield]"
+          />
+        </div>
+      </div>
+      <div className="flex gap-1">
+        <GoClock color="#CBCBCB" />
+        <p className="mb-11 text-gray-6 text-sm">
+          <span className="text-primary">1:59</span> تا دریافت مجدد کد
+        </p>
+      </div>
+      <button className="primary__btn mb-8" onClick={acceptCodeHandler}>
+        تائید
+      </button>
+    </div>
+  );
+};
+
+export default AcceptCode;
--- components/module/Header/Login.tsx
+++ components/module/Header/Login.tsx
@@ -1,25 +1,20 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import LoginBox from "./LoginBox";
+import { GoClock } from "react-icons/go";
+import AcceptCode from "./AcceptCode";
 
 const Login: React.FC = () => {
   const [openCode, setCodeOpen] = useState<boolean>(false);
   const signupHandler = () => {
     setCodeOpen(true);
   };
+
+
   return (
     <div className="z-50 bg-white w-150 h-103 rounded-2xl p-8">
-      {openCode ? (
-        <div>
-          <h4 className="font-bold-shabnam! mb-10 text-center text-2xl text-gray-12">
-            کد تائید
-          </h4>
-          <button className="primary__btn">تائید</button>
-        </div>
-      ) : (
-        <LoginBox signupHandler={signupHandler} />
-      )}
+      {openCode ? <AcceptCode /> : <LoginBox signupHandler={signupHandler} />}
     </div>
   );
 };
