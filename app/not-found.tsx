85be39212eff57b8de3f66f4034fab3f05df0efd
soheil14 <jalilisoheil14@gmail.com>
2025-11-03 14:32:28 -0800

    [EDIT]: responsive not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,19 +4,19 @@ import React from "react";
 
 const NotFound: React.FC = () => {
   return (
-    <div className="pt-16 pb-26 container">
-      <div className="border border-gray-5 w-full h-[654px] rounded-2xl flex flex-col justify-center items-center">
-        <Image src={'/assets/images/not-found.png'} width={457} height={340} alt="Not Found" quality={100} className="mb-[27px]" />
-        <div className="flex flex-col gap-1 justify-center items-center mb-8">
-          <h4 className="text-gray-12 text-2xl font-bold-shabnam!">
+    <div className="pt-12 md:pt-16 pb-14 md:pb-26 px-4 md:px-0 container">
+      <div className="border border-gray-5  mx-auto md:w-[90%] xl:w-full h-[595px] md:h-[654px] rounded-2xl flex flex-col justify-center items-center">
+        <Image src={'/assets/images/not-found.png'} width={457} height={340} alt="Not Found" quality={100} className="mb-[27px] w-[271px] h-[202px] sm:w-[457px] sm:h-[340px]" />
+        <div className="flex flex-col gap-2 md:gap-1 justify-center items-center px-1 md:px-0 mb-8 text-center">
+          <h4 className="text-gray-12 text-xl md:text-2xl font-bold-shabnam!">
             صفحه‌ مورد نظر گم شده!
           </h4>
-          <p className="text-gray-11 text-xl">
+          <p className="text-gray-11 text-sm md:text-xl">
             املاک به‌سرعت در حال خرید و فروش و اجاره‌اند، از صفحه اصلی گزینه مورد نظر را جست‌وجو کنید.
           </p>
         </div>
 
-        <Link href="/" className="primary__btn w-[393px]!">
+        <Link href="/" className="primary__btn w-39! text-xs! md:w-[393px]!">
           بازگشت به صفحه اصلی
         </Link>
 
