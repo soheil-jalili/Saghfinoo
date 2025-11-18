d46008ab48807275bdb1e35db85c6ec11ac300b2
soheil-jalili <jalilisoheil14@gmail.com>
2025-11-18 13:39:38 -0800

    [DELETE] middleware.ts

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextURL } from "next/dist/server/web/next-url";
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  res.headers.set("pathname", req.nextUrl.pathname);
-  return res;
-}
