3685cb86017294b85b3e68d86b7ccd458b8add19
soheil-jalili <jalilisoheil14@gmail.com>
2025-12-05 05:16:50 -0800

    [ADD] add AdNews component

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,6 +3,7 @@ import HousingCard from "@/components/module/HousingCard/HousingCard";
 import TitlePage from "@/components/module/TitlePage/TitlePage";
 import Housing from "@/components/templates/Estate/Housing/Housing";
 import MainNewsItem from "@/components/templates/News/MainNewsItem";
+import AdNews from "@/components/templates/News/AdNews";
 
 const News: React.FC = () => {
   return (
@@ -30,6 +31,9 @@ const News: React.FC = () => {
         title="ساخت و ساز"
         className="mt-10 sm:mt-14! px-4  md:px-10.5 lg:px-[65px] xl:px-0 text-gray-12"
       />
+
+      <AdNews />
+
     </div>
   );
 };
---
+++ components/templates/News/AdNews.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+const AdNews: React.FC = () => {
+  return (
+    <section
+      className="bg-gray-2 rounded-2xl flex flex-col-reverse sm:flex-row sm:items-center justify-between sm:px-8 px-4 gap-8 py-[25.5px] mx-4 md:mx-10.5 lg:mx-[65px] xl:mx-0
+    "
+    >
+      <div>
+        <h3 className="text-gray-12 text-sm md:text-base lg:text-[32px] font-bold-shabnam! mb-4 sm:mb-6">
+          در سقفینو همیشه خانه‌ای منتظر شماست
+        </h3>
+        <p className="text-xs mb-3 sm:mb-16 text-gray-11 md:text-base lg:text-2xl leading-[140%] font-medium-shabnam! w-full xl:w-201.5">
+          چه به‌دنبال پیدا کردن یک خانه دلنشین هستید و یا مدیر آژانس املاک و یا
+          یک مشاور مستقل هستید، ما همیشه کنار شما هستیم.
+        </p>
+      </div>
+      <Image
+        src={"/assets/images/news/illustration.png"}
+        alt="Ad Image"
+        width={290}
+        height={318}
+        className="w-[177px] h-[177px] sm:w-72.5 sm:h-79.5 mx-auto"
+      />
+    </section>
+  );
+};
+
+export default AdNews;
