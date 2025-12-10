ee22cf7c1f49beacd3a605935a65de8b72c0dbbd
soheil-jalili <jalilisoheil14@gmail.com>
2025-12-10 15:20:37 -0800

    [ADD] MainContact Component For Contact Page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,12 @@
+import MainContact from "@/components/templates/Contact/MainContact";
 import React from "react";
 
 const ContactPage: React.FC = () => {
-  return <h1 className="p-16">contact</h1>;
+  return (
+    <section className="xl:container rounded-2xl pt-8 pb-6 border border-gray-5 my-16 mx-4 md:mx-10.5 lg:mx-[65px] ml:mx-0">
+      <MainContact />
+    </section>
+  );
 };
 
 export default ContactPage;
--- components/module/Button/Button.tsx
+++ components/module/Button/Button.tsx
@@ -1,15 +1,29 @@
-import Link from 'next/link'
-import React, { ComponentProps } from 'react'
+import Link from "next/link";
+import React, { ComponentProps } from "react";
 
-type Props = ComponentProps<'a'> & {
-    variant?: 'solid' | 'outline',
-    route?: string
-}
+type Props = ComponentProps<"a"> & {
+  variant?: "solid" | "outline";
+  route?: string;
+};
 
-const Button: React.FC<Props> = ({ className, children, variant = 'solid', route = '/' }: Props) => {
-    return (
-        <Link href={route} className={`${variant === 'outline' ? 'border border-primary text-primary' : 'bg-primary text-white'} flex items-center justify-center text-sm cursor-pointer rounded-lg ${className}`}>{children}</Link>
-    )
-}
+const Button: React.FC<Props> = ({
+  className,
+  children,
+  variant = "solid",
+  route = "/",
+}: Props) => {
+  return (
+    <Link
+      href={route}
+      className={`${
+        variant === "outline"
+          ? "border border-primary text-primary"
+          : "bg-primary text-white"
+      } flex items-center justify-center text-sm cursor-pointer rounded-lg ${className}`}
+    >
+      {children}
+    </Link>
+  );
+};
 
-export default Button
+export default Button;
