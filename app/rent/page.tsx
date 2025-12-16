a7ff995f537574631bcee8d8ff940789cddf14a4
soheil-jalili <jalilisoheil14@gmail.com>
2025-12-16 06:48:59 -0800

    [ADD] another item of RentalCard component to rent page part 3

--- app/rent/page.tsx
+++ app/rent/page.tsx
@@ -13,7 +13,7 @@ const RentPage: React.FC = () => {
       <div className="flex justify-between gap-x-6 mb-14">
         <div className="flex-1">
           <TitlePage title="املاک اجاره‌ای" className="mb-2! mt-0!" />
-          <div className="flex justify-between mb-2">
+          <div className="flex justify-between mb-[27px] sm:mb-8">
             <p className="text-primary text-sm sm:text-lg hidden sm:block">
               ۴۷.۵۰۷ مورد یافت شد
             </p>
@@ -57,7 +57,58 @@ const RentPage: React.FC = () => {
             />
           </div>
 
+          <div className="flex flex-wrap gap-x-6 gap-y-4 justify-between content-start mb-10 sm:mb-14">
+            <RentalCard
+              image={"/assets/images/rent/rent.png"}
+              title="رهن و اجاره آپارتمان نوساز"
+              address="۹۵ متر، محدوه آزادی خیابان جی"
+              mortgage="250 میلیون تومان"
+              rentPrice="15 میلیون تومان"
+              date={"۱ ساعت پیش"}
+            />
+            <RentalCard
+              image={"/assets/images/rent/rent.png"}
+              title="رهن و اجاره آپارتمان نوساز"
+              address="۹۵ متر، محدوه آزادی خیابان جی"
+              mortgage="250 میلیون تومان"
+              rentPrice="15 میلیون تومان"
+              date={"۱ ساعت پیش"}
+            />
+            <RentalCard
+              image={"/assets/images/rent/rent.png"}
+              title="رهن و اجاره آپارتمان نوساز"
+              address="۹۵ متر، محدوه آزادی خیابان جی"
+              mortgage="250 میلیون تومان"
+              rentPrice="15 میلیون تومان"
+              date={"۱ ساعت پیش"}
+            />
+            <RentalCard
+              image={"/assets/images/rent/rent.png"}
+              title="رهن و اجاره آپارتمان نوساز"
+              address="۹۵ متر، محدوه آزادی خیابان جی"
+              mortgage="250 میلیون تومان"
+              rentPrice="15 میلیون تومان"
+              date={"۱ ساعت پیش"}
+            />
+          </div>
+
           <div className="flex flex-wrap gap-x-6 gap-y-4 justify-between content-start">
+            <RentalCard
+              image={"/assets/images/rent/rent.png"}
+              title="رهن و اجاره آپارتمان نوساز"
+              address="۹۵ متر، محدوه آزادی خیابان جی"
+              mortgage="250 میلیون تومان"
+              rentPrice="15 میلیون تومان"
+              date={"۱ ساعت پیش"}
+            />
+            <RentalCard
+              image={"/assets/images/rent/rent.png"}
+              title="رهن و اجاره آپارتمان نوساز"
+              address="۹۵ متر، محدوه آزادی خیابان جی"
+              mortgage="250 میلیون تومان"
+              rentPrice="15 میلیون تومان"
+              date={"۱ ساعت پیش"}
+            />
             <RentalCard
               image={"/assets/images/rent/rent.png"}
               title="رهن و اجاره آپارتمان نوساز"
