3ba9fdfbfb2087e7b84d1eac22179cd4c2c5eb3f
soheil-jalili <jalilisoheil14@gmail.com>
2026-01-05 13:06:34 -0800

    [FIX] SelectBox component like ui

--- components/templates/Rent/FilterRent/FilterRent.tsx
+++ components/templates/Rent/FilterRent/FilterRent.tsx
@@ -8,8 +8,6 @@ const SelectBox = dynamic(() => import("../SelectBoxFilter/SelectBox"), {
   ssr: false,
 });
 
-
-
 const FilterRent: React.FC = () => {
   return (
     <div className="mt-6 lg:mt-18 xl:mt-22 hidden md:flex items-baseline justify-between lg:px-[57px] xl:px-0 gap-x-2">
