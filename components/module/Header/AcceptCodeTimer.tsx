470293957f42015ab3c6d58ecbcac737914201f4
soheil-jalili <jalilisoheil14@gmail.com>
2026-08-12 18:12:04 -0700

    [ADD] timer AcceptCodeTimer.tsx

--- components/module/Header/AcceptCodeTimer.tsx
+++ components/module/Header/AcceptCodeTimer.tsx
@@ -1,20 +1,48 @@
-import React from 'react'
-import { GoClock } from 'react-icons/go'
-
+"use client";
+import React, { useEffect, useState } from "react";
+import { GoClock } from "react-icons/go";
 
 type AcceptCodeTimerType = {
-    fontSize: 'text-sm' | 'text-[10px]'
-}
+  fontSize: "text-sm" | "text-[10px]";
+};
 
 const AcceptCodeTimer: React.FC<AcceptCodeTimerType> = ({ fontSize }) => {
-    return (
-        <div className="flex gap-1">
-            <GoClock color="#CBCBCB" />
-            <p className={`mb-11 text-gray-6 cursor-pointer ${fontSize}`}>
-                <span className="text-primary cursor-auto">1:59</span> تا دریافت مجدد کد
-            </p>
-        </div>
-    )
-}
+  const [timeLeft, setTimeLeft] = useState<number>(180);
 
-export default AcceptCodeTimer
+  useEffect(() => {
+    if (timeLeft <= 0) return;
+
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  const minute = Math.floor(timeLeft / 60);
+  const second = timeLeft % 60;
+  const isDoneTime = timeLeft === 0;
+
+  return (
+    <div className="flex gap-1">
+      <GoClock color="#CBCBCB" />
+      <p className={`mb-11 text-gray-6 cursor-pointer ${fontSize}`}>
+        {isDoneTime ? (
+          <>
+            <button className="cursor-pointer">دریافت مجدد کد</button>
+          </>
+        ) : (
+          <>
+            <span className="text-primary cursor-auto">
+              {minute.toString().padStart(2, "0")}:
+              {second.toString().padStart(2, "0")}
+            </span>{" "}
+            تا دریافت مجدد کد
+          </>
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default AcceptCodeTimer;
